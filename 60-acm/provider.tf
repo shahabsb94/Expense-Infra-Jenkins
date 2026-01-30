ecc86175fb8f624998c72a7ea620136157e822dd
shahabsb94 <shahabsb94@gmail.com>
2026-01-30 18:44:05 +0530

    Jenkins Infra

---
+++ 60-acm/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.84.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "expense-project-dev"
+    key    = "expense-dev-eks-acm-jenkins" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    region = "us-east-1"
+    dynamodb_table = "expense-project-dev"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
