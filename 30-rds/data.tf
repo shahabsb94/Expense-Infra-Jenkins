c7c94331c442ee0c68bc243f4fc93a5f9381efaf
shahabsb94 <shahabsb94@gmail.com>
2026-01-30 12:40:20 +0530

    Jenkins Infra

--- 30-rds/data.tf
+++ 30-rds/data.tf
@@ -9,3 +9,11 @@ data "aws_ssm_parameter" "public_subnet_ids" {
 data "aws_ssm_parameter" "database_subnet_group_name" {
   name = "/${var.project_name}/${var.environment}/database_subnet_group_name"
 }
+
+data "aws_secretsmanager_secret" "db" {
+  name = "expense/dev/mysql"
+}
+
+data "aws_secretsmanager_secret_version" "db" {
+  secret_id = data.aws_secretsmanager_secret.db.id
+}
--- 30-rds/locals.tf
+++ 30-rds/locals.tf
@@ -2,4 +2,7 @@ locals {
   resource_name = "${var.project_name}-${var.environment}"
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
   database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
+  db_password = jsondecode(
+    data.aws_secretsmanager_secret_version.db.secret_string
+  )["password"]
 }
